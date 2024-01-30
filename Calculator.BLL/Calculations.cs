e62fd0ed2f0b51493cccccf8a8e1fe48d6066097
Lucia <Luciaabouhamad@gmail.com>
2024-01-30 11:29:06 +0100

    Added Switch case method

--- Calculator.BLL/Calculations.cs
+++ Calculator.BLL/Calculations.cs
@@ -8,9 +8,53 @@ namespace Calculator.BLL
 {
 	public class Calculations
 	{
-		public float Add(float firstnumber, float secoundnumber) 
+		public float? Calculate(string calcMethod, float firstnumber, float secondnumber)
+		{
+			switch (calcMethod)
+			{
+				case "add":
+				{
+					return Add(firstnumber, secondnumber);
+				}
+				case "subtract":
+				{
+					return Subtract(firstnumber,secondnumber);
+				}
+				case "multiply":
+				{
+					return Multiply(firstnumber,secondnumber);
+				}
+				case "divide":
+				{
+					return Divide(firstnumber,secondnumber);
+				}
+				default:
+				{
+					return null;
+				}
+			}
+		}
+
+		//Calculation methods
+		//ADD
+		private float Add(float firstnumber, float secondnumber) 
 		{ 
-		    return firstnumber + secoundnumber;
+		    return firstnumber + secondnumber;
+		}
+		//Subtract
+		private float Subtract(float firstnumber, float secondnumber) 
+		{ 
+		    return firstnumber - secondnumber;
+		}
+		//Multiply
+		private float Multiply(float firstnumber, float secondnumber) 
+		{ 
+		    return firstnumber * secondnumber;
+		}
+		//Divide
+		private float Divide(float firstnumber, float secondnumber) 
+		{ 
+		    return firstnumber / secondnumber;
 		}
 	}
 }
